d3c0341bc11e9e5060f3693188d3c6410f5ab2c0
wski <contact.mattdylan@gmail.com>
2016-02-04 16:25:40 -0500

    feat(Nest): unregister nest

---
+++ src/app/modules/nest/unregisterNest.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+let Nest = mongoose.model('Nest');
+
+/** @function
+ * @name unregisterNest
+ * @param {object} options - Server information
+ * @param {string} options.address - The address of the server.
+ * @param {string} options.owner - The owner of the server.
+ * @return {promise} promise - Returns new promise.
+ */
+const unregisterNest = function(options) {
+  return new Promise((resolve, reject) => {
+    let query = Nest.findOne({address: options.address});
+
+    query.find(function(err, nests) {
+      if (err) {
+        reject(new Error(
+          'Unknown Mongoose issue.',
+          'nest/addrole.js',
+          '20'
+        ));
+      }
+      if (!nests.length) {
+        resolve({
+          status: 404,
+          data: {
+            error: `Machine not found.`
+          }
+        });
+      } else {
+        let nest = nests[0];
+        nest.remove(err => {
+          if (err) {
+            resolve({
+              status: 500,
+              data: {
+                error: `Error removing machine.`
+              }
+            });
+          } else {
+            resolve({
+              status: 404,
+              data: {
+                error: `Machine removed from Meep.`
+              }
+            });
+          }
+        });
+      }
+    });
+  });
+};
+
+module.exports = unregisterNest;
