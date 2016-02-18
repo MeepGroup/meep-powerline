3d40dd7b9060143bc35d4344b5ee312651d096ce
wski <contact.mattdylan@gmail.com>
2016-02-17 22:31:22 -0500

    feat(instance): Added instance routes, needs testing

---
+++ src/app/routes/instance.js
@@ -0,0 +1,96 @@
+/* This file is subject to the terms and conditions defined in
+   file 'LICENSE.md', which is part of this source code package. */
+'use strict';
+
+const request = require('request');
+
+const {
+  getAuthKey
+} = require('../modules/');
+
+const spawn = async function(req, res) {
+  let options = req.body;
+  options.owner = req.user.local.email;
+  let authKeyData = await getAuthKey({
+    address: req.body.address,
+    owner: req.user.local.email
+  });
+  options.authKey = authKeyData.data.authKey;
+
+  request.post(`http://{options.address}/spawn`, {
+    form: options
+  }, (err, httpResponse, body) => {
+    if (err) {
+      res.status(500).jsonp(body);
+    } else {
+      res.status(200).jsonp(body);
+    }
+  });
+};
+
+const despawn = async function(req, res) {
+  let options = req.body;
+  options.owner = req.user.local.email;
+  let authKeyData = await getAuthKey({
+    address: req.body.address,
+    owner: req.user.local.email
+  });
+  options.authKey = authKeyData.data.authKey;
+
+  request.post(`http://{options.address}/despawn`, {
+    form: options
+  }, (err, httpResponse, body) => {
+    if (err) {
+      res.status(500).jsonp(body);
+    } else {
+      res.status(200).jsonp(body);
+    }
+  });
+};
+
+const cycle = async function(req, res) {
+  let options = req.body;
+  options.owner = req.user.local.email;
+  let authKeyData = await getAuthKey({
+    address: req.body.address,
+    owner: req.user.local.email
+  });
+  options.authKey = authKeyData.data.authKey;
+
+  request.post(`http://{options.address}/cycle`, {
+    form: options
+  }, (err, httpResponse, body) => {
+    if (err) {
+      res.status(500).jsonp(body);
+    } else {
+      res.status(200).jsonp(body);
+    }
+  });
+};
+
+const instance = async function(req, res) {
+  let options = req.body;
+  options.owner = req.user.local.email;
+  let authKeyData = await getAuthKey({
+    address: req.body.address,
+    owner: req.user.local.email
+  });
+  options.authKey = authKeyData.data.authKey;
+
+  request.post(`http://{options.address}/instance`, {
+    form: options
+  }, (err, httpResponse, body) => {
+    if (err) {
+      res.status(500).jsonp(body);
+    } else {
+      res.status(200).jsonp(body);
+    }
+  });
+};
+
+module.exports = {
+  spawn,
+  despawn,
+  cycle,
+  instance
+};
