f02806c03b2d53b98ef7bab5ac0f743b0ed3864d
Matt Wisniewski <contact.mattdylan@gmail.com>
2016-02-29 13:56:23 -0500

    Update owncloud_ubuntu.yolk.js

--- src/yolks/owncloud_ubuntu.yolk.js
+++ src/yolks/owncloud_ubuntu.yolk.js
@@ -2,10 +2,9 @@
 const Yolk = require('./yolk_transpiler.js');
 
 const owncloud_ubuntu = new Yolk(`
-sudo cd /tmp
-sudo wget http://download.opensuse.org/repositories/isv:ownCloud:community/xUbuntu_14.04/Release.key
-sudo apt-key add - < Release.key
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/community/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud.list"
+sudo cd /tmp && sudo wget http://download.opensuse.org/repositories/isv:ownCloud:community/xUbuntu_14.04/Release.key
+sudo cd /tmp && sudo apt-key add - < Release.key
+sudo cd /tmp && sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/community/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud.list"
 sudo apt-get update -y
 sudo apt-get install -y owncloud
 `,
